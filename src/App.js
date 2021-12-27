eeeed6d6b3161a62c56e49cc0dc3097fe013acb5
Aditykumar <adityakushwah29@gmail.com>
2021-12-27 17:06:56 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
 import './components/Style.css';
 import Employe from './components/Employe'; 
-// import {BrowserRouter as Router,Routes, Route } from 'react-router-dom';
-// import Display from './components/Display';
+
 
 function App() {
   return (
     <div className="App">
-    {/* <Router>
-      <Routes>
-        <Route path='/' element={<Employe/>}/>
-        <Route path=':display' element={<Display/>}/>
-      </Routes>
-
-    </Router> */}
+  
     <Employe/>
     </div>
   );
